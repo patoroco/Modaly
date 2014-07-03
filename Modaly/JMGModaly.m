d5b7ecef21f449004b19992213f66f72e56b89e1
Jorge Maroto <patoroco@gmail.com>
2014-07-03 18:44:41 +0200

    Preparing file structure

---
+++ Modaly/JMGModaly.m
@@ -0,0 +1,13 @@
+//
+//  JMGModaly.m
+//  Modaly
+//
+//  Created by Jorge Maroto García on 03/07/14.
+//  Copyright (c) 2014 Jorge Maroto García. All rights reserved.
+//
+
+#import "JMGModaly.h"
+
+@implementation JMGModaly
+
+@end
